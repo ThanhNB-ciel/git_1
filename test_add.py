a3525dfa14c394dd931d00d82ce708bc39ad4456
ThanhNB-ciel <baothanh1371997@gmail.com>
2023-03-16 17:07:29 +0700

    commit

---
+++ test_add.py
@@ -0,0 +1,6 @@
+# from google.colab import drive
+# drive.mount('/content/gdrive')
+from clickhouse_driver import Client
+import pandas as pd
+from datetime import date, timedelta, datetime
+import json
